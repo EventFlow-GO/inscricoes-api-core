82a0e20dbb337beba82d851eceb0c4feb4afd771
9Neres <ricardoborgescontact@gmail.com>
2026-06-04 22:15:45 -0300

    Feat: Created InscricaoService.java

---
+++ src/main/java/com/eventflow/service/InscricaoService.java
@@ -0,0 +1,37 @@
+package com.eventflow.service;
+
+import com.eventflow.dto.InscricaoRequestDTO;
+import com.eventflow.model.InscricaoModel;
+import com.eventflow.producer.InscricaoProducer;
+import com.eventflow.repository.InscricaoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class InscricaoService {
+
+    private final InscricaoRepository repository;
+    private final InscricaoProducer producer;
+
+    // Injeção de dependência via construtor
+    public InscricaoService(InscricaoRepository repository, InscricaoProducer producer) {
+        this.repository = repository;
+        this.producer = producer;
+    }
+
+    @Transactional
+    public InscricaoModel processarNovaInscricao(InscricaoRequestDTO request) {
+
+        InscricaoModel novaInscricao = new InscricaoModel();
+        novaInscricao.setNome(request.nome());
+        novaInscricao.setEmail(request.email());
+        novaInscricao.setSenha(request.senha());
+
+        InscricaoModel inscricaoSalva = repository.save(novaInscricao);
+
+        // Publica a mensagem na Fila
+        producer.enviarIncricao(request);
+
+        return inscricaoSalva;
+    }
+}
